224889e0d2c2db8fc472c9541d4ac512086d1038
Jaiyant Jakhar <77625246+Jjakhar@users.noreply.github.com>
2024-04-22 17:35:34 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 123
+let accountEmail = "jaiyantjakhar@gmail.com"
+var accountPassword = "abcd"
+accountCity = "Delhi"
+let accountState
+
+// prefer not to use var because of issue of block scope and operational scope
+
+console.log(accountId)
+console.table([accountEmail, accountPassword, accountCity, accountState])
