159b4fc04ae234a11780cdbc4998c3059f536b05
Jaiyant Jakhar <77625246+Jjakhar@users.noreply.github.com>
2024-04-24 16:27:55 +0000

    Datatype summary

---
+++ 01_basics/datatypeSummary.js
@@ -0,0 +1,19 @@
+// Primitive
+
+/*
+7 types : number, BigInt, string, boolean, null, undefined, symbol
+*/
+
+// Reference (non primitive)
+// objects, arrays, functions
+
+let myObj = {
+    name : "Jaiyant", 
+    age : 20
+}
+
+let myArray = [1, 2, 3]
+
+let myFun = function(){
+    console.log("Hello World")
+}
