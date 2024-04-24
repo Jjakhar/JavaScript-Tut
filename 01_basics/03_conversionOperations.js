7bd783e4f82c1ac8d63ef008f7c31991e8e42bff
Jaiyant Jakhar <77625246+Jjakhar@users.noreply.github.com>
2024-04-24 15:39:58 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,32 +1,54 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-/* 
-when converting into number
-"33" => 33
-"33abc" = NaN
-null => 0
-undefined => NaN
-true => 1 and false => 0
-*/
+// /* 
+// when converting into number
+// "33" => 33
+// "33abc" = NaN
+// null => 0
+// undefined => NaN
+// true => 1 and false => 0
+// */
 
-/*
-when converting into boolean
-1 -> true, 0 -> false
-"" -> false, "af" -> true
-*/
+// /*
+// when converting into boolean
+// 1 -> true, 0 -> false
+// "" -> false, "af" -> true
+// */
 
-let someNumber = 1213
-let toString = String(someNumber)
+// let someNumber = 1213
+// let toString = String(someNumber)
 
-console.log(typeof toString)
-console.log(toString)
+// console.log(typeof toString)
+// console.log(toString)
 
+
+// ************************** operatoins *********************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2 * 3)
+// console.log(2 ** 3) // 2 to the power of 3
+
+let str1 = "Hello"
+let str2 = " Jaiyant"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
 
