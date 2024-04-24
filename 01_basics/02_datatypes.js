e7a24b1ad1098f6a09e0998e0e639bb30fb0d5ca
Jaiyant Jakhar <77625246+Jjakhar@users.noreply.github.com>
2024-04-24 15:08:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code a newer version
+
+// alert(3 + 3) // Gives error because we are using node not browser
+
+let name = "Jaiyant Jakhar"
+let age = 20
+let isLoggedIn = false
+
+/* number 2 to the power 53
+bigint
+string = ""
+boolean = true or false
+null => standlone value
+undefined
+symbol => unique
+
+user-defined => object
+*/
+
+console.log(typeof age)
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
