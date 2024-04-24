0c30e19bc1809a68aff1652ff6cd90b912f5133a
Jaiyant Jakhar <77625246+Jjakhar@users.noreply.github.com>
2024-04-24 15:59:57 +0000

    comparison

---
+++ 01_basics/04_conversions.js
@@ -0,0 +1,13 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// avoid comparisons among different datatypes
+// equality check (==) and comparison (>, <, <=, >=) works differently in JS
+
+// Strict check (===) :
+
+// console.log("2" === 2)
