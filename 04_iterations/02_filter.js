d9100150d03c32e19a8e28a1c4996f1faf096256
Jaiyant Jakhar <77625246+Jjakhar@users.noreply.github.com>
2024-05-14 17:11:31 +0000

    filter

---
+++ 04_iterations/02_filter.js
@@ -0,0 +1,23 @@
+// for each loop does not returns anything
+
+// const coding = ['c', 'c++', 'js', 'python']
+
+// // console.log(coding)
+
+// const val = coding.forEach( (item) => {
+//     console.log(item)
+//     return item
+// })
+
+// console.log(val) // gives undefined
+
+// filtes also returns
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const newNums = nums.filter( (num) => (num > 4) )
+
+console.log(newNums)
+
+
+
